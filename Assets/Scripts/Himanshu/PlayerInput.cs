83f798b86a596947bf889681904f20d226ab2c6e
himchawla <58020143+himchawla@users.noreply.github.com>
2021-08-28 01:56:16 +1200

    Close to full game loop

--- Assets/Scripts/Himanshu/PlayerInput.cs
+++ Assets/Scripts/Himanshu/PlayerInput.cs
@@ -11,11 +11,20 @@ namespace Himanshu
         private float m_movement;
         private bool m_handBrake;
         private bool m_defaultBoost;
-        
+        private float m_rightAnalog;
+        private float m_rightAnalogVert;
+
         
         [SerializeField] private int playerIndex;
         private bool m_powerUp;
         private bool m_powershotBoost;
+
+
+        //Returns right analog stick
+        public float rightStick => Mathf.Abs(m_rightAnalog) > 0.1f ? m_rightAnalog : 0;
+        
+        
+        //Returns true if powerup button pressed
         public bool powerUp => m_powerUp;
 
         public int index => playerIndex;
@@ -28,6 +37,7 @@ namespace Himanshu
 
         
         public bool defaultBoost => m_defaultBoost;
+        
         public bool powershotBoost => m_powershotBoost;
 
         //returns throttle +ve if r2 is pressed
@@ -35,7 +45,7 @@ namespace Himanshu
         
         //returns brake +ve if l2 is pressed
         public float brake => m_brake > 0 ? m_brake : 0;
-
+        public float rightStickVert => Mathf.Abs(m_rightAnalogVert) > 0.1f ? m_rightAnalogVert : 0;
 
 
         private void Start()
@@ -56,6 +66,8 @@ namespace Himanshu
             m_defaultBoost = Input.GetButton($"Boost_P{playerIndex}");
             m_powershotBoost = Input.GetButtonDown($"Boost_P{playerIndex}");
             m_powerUp = Input.GetButtonDown($"PowerUp_P{playerIndex}");
+            m_rightAnalog = Input.GetAxis($"RightAnalog_P{playerIndex}");
+            m_rightAnalogVert = Input.GetAxis($"RightAnalogVert_P{playerIndex}");
         }
         private void OnDisable()
         {
@@ -68,7 +80,7 @@ namespace Himanshu
             m_brake = 0;
             m_movement = 0;
             m_handBrake = false;
+            m_rightAnalog = 0f;
         }
     }
-    
 }
